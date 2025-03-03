646237cd7f117ad2905ed1674a61c81f49f61cc6
karlvbiron <karl.v.biron@gmail.com>
2025-03-04 00:41:59 +0800

    Create Convert_a_Boolean_to_a_String.rb

---
+++ Ruby_Katas/8_kyu/Convert_a_Boolean_to_a_String.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+############
+# SOLUTION #
+############
+
+def boolean_to_string(b)
+  return b.to_s
+end
+
+
+################
+# SAMPLE TESTS #
+################
+=begin
+Test.describe "Basic Tests" do
+  Test.it "should pass basic tests" do
+    Test.assert_equals(boolean_to_string(true), "true")
+    Test.assert_equals(boolean_to_string(false), "false")
+  end
+end
+=end
+
+################
+# INSTRUCTIONS #
+################
+=begin
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+=end
+
+###################
+# ATTEMPT RESULTS #
+###################
+
+=begin
+Test Results:
+Basic Tests
+should pass basic tests
+Test Passed: Value == "true"
+Test Passed: Value == "false"
+You have passed all of the tests! :)
+=end
